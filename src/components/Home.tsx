1fe98d4f437a80770d34bcb7d0d6760a4b15cf53
markperdon <markperdon712@gmail.com>
2023-12-03 09:18:21 +0800

    added users table and login feature

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,8 @@
+import React, { useEffect, useState, useContext } from 'react';
 import axios from 'axios';
 import $ from "jquery";
-import { useEffect, useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import { useNavigate } from 'react-router-dom';
+import { Button, Nav, Navbar } from 'react-bootstrap';
 import AddUser from './shared/AddUser';
 import { titlizeText } from './shared/global';
 
@@ -14,31 +12,35 @@ interface User {
   username: string;
   accountType: string;
 }
-const Home = () => {
+
+const Home: React.FC = () => {
   const navigate = useNavigate();
-  const [authenticated, setAuthenticated] = useState(false);
+
+  const [authenticated, setAuthenticated] = useState<boolean>(false);
   const token = localStorage.getItem('token');
   const username = localStorage.getItem('username');
   const accountType = localStorage.getItem('accountType');
   const branchId = localStorage.getItem('branchId');
   const userId = localStorage.getItem('userId');
-  const [showModal, setShowModal] = useState(branchId === '');
-  const handleShow = () => setShowModal(true);
-  const handleClose = () => setShowModal(false);
+
+  const [showModal, setShowModal] = useState<boolean>(branchId === '');
   const [usersList, setUsersList] = useState<User[] | null>(null);
 
   useEffect(() => {
-    // Check if the user is authenticated (you can implement a more secure check)
-    const isAuthenticated = !!token;
+    const checkAuthentication = async () => {
+      const isAuthenticated = !!token;
 
-    if (!isAuthenticated) {
-      // Redirect to the login page if not authenticated
-      navigate('/login');
-    }
-    getUsers();
-    setAuthenticated(isAuthenticated);
+      if (!isAuthenticated) {
+        navigate('/login');
+      } else {
+        getUsers();
+        setAuthenticated(true);
+      }
+    };
 
+    checkAuthentication();
   }, [navigate, token]);
+
   const getUsers = async () => {
     try {
       const response = await axios.get<User[]>('http://localhost:5000/users/all');
@@ -48,120 +50,120 @@ const Home = () => {
     }
   };
 
-  const handleLogout = async () => {
-    // Clear user information from localStorage
-    localStorage.removeItem('token');
-    localStorage.removeItem('username');
-    localStorage.removeItem('branchId');
-    localStorage.removeItem('accountType');
-    // Redirect to the login page after logout
+  const handleLogout = () => {
+    localStorage.clear();
     navigate('/login');
   };
+
   const handleDelete = async (userId: number) => {
     try {
       const response = await axios.delete(`http://localhost:5000/users/${userId}`);
-      const updatedUsers = usersList && usersList.filter((user) => user.id !== userId);
-      if(response.data.success){
+      if (response.data.success) {
+        const updatedUsers = usersList && usersList.filter((user) => user.id !== userId);
         setUsersList(updatedUsers);
       }
-      console.log(response.data)
+      console.log(response.data);
     } catch (error) {
-      console.error('Error fetching users');
+      console.error('Error deleting user');
     }
   };
+
+  const toggleConfirmation = (userId: number, show: boolean) => {
+    $(`#confirm${userId}, #back${userId}, #lbl-del${userId}`).toggle(100);
+    $(`#del${userId}`).toggle(100);
+  };
+
   const handleConfirm = (userId: number) => {
-    console.log(userId, $('#confirm'+userId))
-    $('#confirm'+userId).show();
-    $('#back'+userId).show();
-    $('#lbl-del'+userId).show();
-    $('#del'+userId).hide();
-  }
+    toggleConfirmation(userId, true);
+  };
+
   const handleBack = (userId: number) => {
-    console.log(userId, $('#confirm'+userId))
-    $('#confirm'+userId).hide(100);
-    $('#back'+userId).hide(100);
-    $('#lbl-del'+userId).hide(100);
-    $('#del'+userId).show(100);
-  }
+    toggleConfirmation(userId, false);
+  };
 
-  return (
+  return authenticated ? (
     <div>
-      {authenticated ? (
-        <div>
-          <Navbar bg="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow" expand="lg">
-            <Navbar.Brand>Accounting StartUP</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse className='justify-content-end' id="basic-navbar-nav">
-              <Nav className="justify-content-end">
-                <Button variant="secondary outline-primary btn-sm" onClick={handleLogout}>
-                  Sign out
-                </Button>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-          <div className="container-fluid mt-5">
-
-            <div className="alert alert-light" role="alert">
-            <h2>Welcome, {username && titlizeText(username)}! <button type="button" className="btn btn-outline-success" disabled>{accountType}</button> </h2></div>
-            {accountType === 'admin' && <Button variant="primary btn-sm" onClick={handleShow}>
-                <span className="bi bi-person-add"></span> &nbsp;
-                Add New User
-            </Button> }
-            <div className="table-responsive small tbl-users">
-              <table className="table table-striped table-sm">
-                <thead>
-                  <tr>
-                    <th scope="col">Branch ID</th>
-                    <th scope="col">Username</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Position</th>
-                    {accountType === 'admin' && <th scope="col">Action</th>}
-                    <th scope="col"></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {usersList && usersList.map((user) => {
-                    return (
-                      <tr key={user.id}>
-                        <td>{user.branchId}</td>
-                        <td>{titlizeText(user.username)}</td>
-                        <td>{titlizeText(user.username)}</td>
-                        <td>{user.accountType}</td>
-                        {accountType === 'admin'
-                        && <td><span className='confirm-btn' id={"lbl-del" + user.id}>Delete?</span>
-                          <Button className='btn btn-danger' id={"del" + user.id} onClick={() => handleConfirm(user.id)}>
-                            <i className='bi bi-person-dash'/>
-                          </Button>
-
-                          </td>}
-                          <td><Button className='btn btn-warning confirm-btn' id={"confirm" + user.id} onClick={() => handleDelete(user.id)}>
-                            <i className='bi bi-check-circle-fill'/> Yes
-                          </Button> &nbsp;
-                          <Button className='btn btn-success back-btn' id={"back" + user.id} onClick={() => handleBack(user.id)}>
-                            <i className='bi bi-arrow-counterclockwise'/> No
-                          </Button></td>
-
-                      </tr>
-                    )
-                  })}
-                </tbody>
-              </table>
-            </div>
-          </div>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand>Accounting StartUP</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav className="justify-content-end">
+            <Button variant="secondary outline-primary btn-sm" onClick={handleLogout}>
+              Sign out
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <div className="container-fluid mt-5">
+        <div className="alert alert-light" role="alert">
+          <h2>
+            Welcome, {username && titlizeText(username)}!{' '}
+            <button type="button" className="btn btn-outline-success" disabled>
+              {accountType}
+            </button>
+          </h2>
         </div>
-      ) : (
-        <p>Redirecting to login...</p>
-      )}
-          <AddUser
-            show={showModal}
-            userName={username}
-            branchId={branchId}
-            currentAccountType={accountType}
-            handleClose={handleClose}
-            getUsers={getUsers}
-            userId={userId}
-            />
+        {accountType === 'admin' && (
+          <Button variant="primary btn-sm" onClick={() => setShowModal(true)}>
+            <span className="bi bi-person-add"></span> &nbsp; Add New User
+          </Button>
+        )}
+        <div className="table-responsive small tbl-users">
+          <table className="table table-striped table-sm">
+            <thead>
+              <tr>
+                <th scope="col">Branch ID</th>
+                <th scope="col">Username</th>
+                <th scope="col">Name</th>
+                <th scope="col">Position</th>
+                {accountType === 'admin' && <th scope="col">Action</th>}
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {usersList?.map((user) => (
+                <tr key={user.id}>
+                  <td>{user.branchId}</td>
+                  <td>{titlizeText(user.username)}</td>
+                  <td>{titlizeText(user.username)}</td>
+                  <td>{user.accountType}</td>
+                  {accountType === 'admin' && (
+                    <td>
+                      <span className="confirm-btn" id={`lbl-del${user.id}`}>
+                        Delete?
+                      </span>
+                      <Button className="btn btn-danger" id={`del${user.id}`} onClick={() => handleConfirm(user.id)}>
+                        <i className="bi bi-person-dash" />
+                      </Button>
+                    </td>
+                  )}
+                  <td>
+                    <Button className="btn btn-warning confirm-btn" id={`confirm${user.id}`} onClick={() => handleDelete(user.id)}>
+                      <i className="bi bi-check-circle-fill" /> Yes
+                    </Button>{' '}
+                    &nbsp;
+                    <Button className="btn btn-success back-btn" id={`back${user.id}`} onClick={() => handleBack(user.id)}>
+                      <i className="bi bi-arrow-counterclockwise" /> No
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <AddUser
+        show={showModal}
+        userName={username}
+        branchId={branchId}
+        currentAccountType={accountType}
+        handleClose={() => setShowModal(false)}
+        getUsers={getUsers}
+        userId={userId}
+      />
     </div>
+  ) : (
+    <p>Redirecting to login...</p>
   );
 };
 
